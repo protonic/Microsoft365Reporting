6c91cc18fa687d090e16cf8efc5d7d6fcb33dd07
Pritam Khedekar <pritamkhedekarr@gmail.com>
2021-05-29 14:14:56 +0530

    MS 365 Transport rule changes audit script

--- Get-Office365AdminAuditLogsForLast24Hours.ps1
+++ Get-Office365AdminAuditLogsForLast24Hours.ps1
@@ -29,7 +29,7 @@
 #>
 
 
-################# Import Module ExchangeOnlineManagement ###################
+################# Import Module ExchangeOnlineManagement ####################
 Import-Module ExchangeOnlineManagement
 
 
